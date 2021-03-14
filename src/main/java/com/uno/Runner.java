852074d234a8ea26c2080429c3e99b500919975b
ryanz <ryanz@AMAZON-ECHO.lan>
2021-03-13 23:10:00 -0600

    still updating event queue thing

--- src/main/java/com/uno/Runner.java
+++ src/main/java/com/uno/Runner.java
@@ -1,15 +1,14 @@
 package com.uno;
 import java.awt.EventQueue;
-import java.lang.reflect.InvocationTargetException;
 
 import javax.swing.SwingUtilities;
 
 public class Runner 
 {
-	public static void main(String args[]) throws InvocationTargetException, InterruptedException
+	public static void main(String args[]) 
 	{
 		//game.start();
-		EventQueue.invokeAndWait(new Runnable() {
+		EventQueue.invokeLater(new Runnable() {
 		    public void run() {
 		    	Board game = new Board();
 				GameState gamestate = new GameState(game);
